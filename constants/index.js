573d9a507e70c6ef73e57ab10b07b0243278d4fb
meysamvazifeh72 <139198696+meysamvazifeh72@users.noreply.github.com>
2023-07-20 09:17:57 +0400

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0d17014799a7c6bc9ab9a2e501e12a72a48b880a";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
